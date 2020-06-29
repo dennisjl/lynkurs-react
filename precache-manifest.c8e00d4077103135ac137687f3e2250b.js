beee12c81dfe1f3d95110d1e59ca931416ceda78
Dennis Jianbin Liang <dennisjianbin@gmail.com>
2020-06-29 13:43:05 +0200

    Updates

---
+++ precache-manifest.c8e00d4077103135ac137687f3e2250b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64a11772601f898386fa9c861a676ef",
+    "url": "/lynkurs-react/index.html"
+  },
+  {
+    "revision": "b6b0804c0769b300d0d6",
+    "url": "/lynkurs-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "528a12ae430ebba425d2",
+    "url": "/lynkurs-react/static/js/2.e635f3e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lynkurs-react/static/js/2.e635f3e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b0804c0769b300d0d6",
+    "url": "/lynkurs-react/static/js/main.4c867c18.chunk.js"
+  },
+  {
+    "revision": "ca7e1488469bb9ed9e7c",
+    "url": "/lynkurs-react/static/js/runtime-main.5c8d3050.js"
+  }
+]);
